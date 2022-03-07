686a76caff0fcd642dc7d3a8f654eae7828bdb85
Sumaer Jolly <sumaerjolly@gmail.com>
2022-03-07 13:29:52 +0530

    add connection and tests

---
+++ src/Provider/HarvestResourceOwner.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace SumaerJolly\OAuth2\Client\Provider;
+
+use League\OAuth2\Client\Provider\ResourceOwnerInterface;
+use League\OAuth2\Client\Tool\ArrayAccessorTrait;
+
+class HarvestResourceOwner implements ResourceOwnerInterface
+{
+  use ArrayAccessorTrait;
+
+  /**
+   * @var array
+   */
+  protected $response;
+
+  /**
+   * @param array $response
+   */
+  public function __construct(array $response)
+  {
+    $this->response = $response;
+  }
+
+  public function getId()
+  {
+  }
+  public function getEmail()
+  {
+    return;
+  }
+
+  public function toArray()
+  {
+    return $this->response;
+  }
+}
